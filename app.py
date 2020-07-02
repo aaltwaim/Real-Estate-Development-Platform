6c9e6ead6afbe33cf0b596e60b52e3b77d2393ab
aaltwaim <a_t1411@hotmail.com>
2020-07-02 15:40:57 +0300

    Third-Party Authentication using Auth0

--- app.py
+++ app.py
@@ -1,21 +1,219 @@
 import os
 from flask import Flask, request, abort, jsonify, render_template, session, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_cors import CORS
+from auth import AuthError, requires_auth, requires_signed_in
+from authlib.flask.client import OAuth
+import constants
 import json
 
 
-  # create and configure the app
+AUTH0_CLIENT_ID = constants.AUTH0_CLIENT_ID
+AUTH0_CLIENT_SECRET = constants.AUTH0_CLIENT_SECRET
+AUTH0_CALLBACK_URL = constants.AUTH0_CALLBACK_URL
+AUTH0_DOMAIN = constants.AUTH0_DOMAIN
+AUTH0_AUDIENCE = constants.AUTH0_AUDIENCE
+AUTH0_BASE_URL = 'https://' + constants.AUTH0_DOMAIN
+
+# create and configure the app
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='postgres://aaltwaim@localhost:5432/estate'
 CORS(app)
+db = SQLAlchemy(app)
+migrate= Migrate(app, db)
 from models import Building, Unit
 
+oauth = OAuth(app)
+auth0 = oauth.register(
+    'auth0',
+    client_id=AUTH0_CLIENT_ID,
+    client_secret=AUTH0_CLIENT_SECRET,
+    api_base_url=AUTH0_BASE_URL,
+    access_token_url=AUTH0_BASE_URL + '/oauth/token',
+    authorize_url=AUTH0_BASE_URL + '/authorize',
+    client_kwargs={
+        'scope': 'openid profile email',
+    }
+)
+
+# main page
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return auth0.authorize_redirect(redirect_uri=AUTH0_CALLBACK_URL, audience= AUTH0_AUDIENCE)
 
+@app.route('/login-results')
+def callback_handling():
+    res = auth0.authorize_access_token()
+    token = res.get('access_token')
+    session['jwt_token'] = token
+
+    return redirect('/buildings')
+
+# implement endpoint
+# Get /buildings
+@app.route('/buildings', methods=['GET'])
+def get_buildings():
+    try:
+        buildings = Building.query.all()
+        return jsonify({
+            'success': True,
+            'buildings': [building.show() for building in buildings],
+        })
+    
+    except Exception:
+        abort(404)
+# implement endpoint
+# Get /buildings/<id>
+@app.route('/buildings/<id>', methods=['GET'])
+@requires_auth('get:building-by-id')
+def get_building_by_id(jwt, id):
+    try:
+        building = Building.query.filter(Building.id == id).one_or_none()
+        return jsonify({
+            'success': True,
+            'building': [building.show()]
+        })
+    except Exception:
+        abort(404)
+
+# implement endpoint
+# Post /buildings
+@app.route('/buildings', methods=['POST'])
+@requires_auth('post:buildings')
+def add_building(jwt):
+    body = request.get_json()
+    ownerID = body.get('ownerID')
+    name = body.get('name')
+    address = body.get('address')
+    description = body.get('description')
+    number_of_units = body.get('number_of_units')
+    building_image = body.get('building_image')
+
+    if not ('ownerID' in body and 'name' in body and 'address' in body and 'description' in body
+     and 'number_of_units' in body and 'building_image' in body):
+        abort(422)
+    try:
+        new_building = Building(ownerID=ownerID, name=name, address=address, 
+        description=description, number_of_units=number_of_units, building_image=building_image)
+        new_building.insert()
+        return jsonify({
+            'success': True,
+            'created': [new_building.show()],
+        })
+    except Exception:
+        abort(422)
+
+# implement endpoint
+# Patch /buildings
+@app.route('/buildings/<id>', methods=['PATCH'])
+@requires_auth('patch:buildings')
+def update_building(jwt, id):
+    
+    building = Building.query.filter(Building.id == id).one_or_none()
+    if building:
+        try:
+            body = request.get_json()
+            ownerID = body.get('ownerID')
+            name = body.get('name')
+            address = body.get('address')
+            description = body.get('description')
+            number_of_units = body.get('number_of_units')
+            building_image = body.get('building_image')
+            if ownerID:
+                building.ownerID = ownerID
+            if name:
+                building.name = name
+            if address:
+                building.address = address
+            if description:
+                building.description = description
+            if number_of_units:
+                building.number_of_units = number_of_units
+            if building_image:
+                building.building_image = building_image
+            building.update()
+            return jsonify({
+                'success': True,
+                'buildings': [building.show()],
+            })
+        except Exception:
+            abort(422)
+    else:
+        abort(404)
+
+# implement endpoint
+# delete /buildings/<id>
+@app.route('/buildings/<id>', methods=['DELETE'])
+@requires_auth('delete:buildings')
+def delete_building(jwt, id):
+    building = Building.query.get(id)
+    if building:
+        try:
+            building.delete()
+            return jsonify({
+                'success': True,
+                'delete': id,
+            })
+        except Exception:
+            abort(422)
+    else:
+        abort(404)
+
+# implement endpoint
+# Get /buildings/<id>/units
+@app.route('/buildings/<int:building_id>/units', methods=['GET'])
+def get_units_based_on_building(building_id):
+    try:
+        units_by_building = Unit.query.filter(Unit.building_id == building_id).all()
+        return jsonify({
+            'success': True,
+            'buildings': [unit.show() for unit in units_by_building],
+        })
+    
+    except Exception:
+        abort(404)
+
+# implement error handler 422
+@app.errorhandler(422)
+def unprocessable(error):
+    return jsonify({
+                    "success": False,
+                    "error": 422,
+                    "message": "unprocessable"
+                    }), 422
+
+
+# implement error handler 404
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({
+                    "success": False,
+                    "error": 404,
+                    "message": "not found"
+                    }), 404
+
+# implement error handler for AuthError
+@app.errorhandler(AuthError)
+def auth_error_handler(ex):
+    return jsonify({
+                    "success": False,
+                    "error": ex.status_code,
+                    "message": ex.error
+                    }), 401
+
+# implement error handler 400
+@app.errorhandler(400)
+def bad_request(error):
+    return jsonify({
+                    "success": False,
+                    "error": 400,
+                    "message": "bad request"
+                    }), 400
 
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
--- models.py
+++ models.py
@@ -1,21 +1,29 @@
 from app import db
+from flask_sqlalchemy import SQLAlchemy
 import json
 
+
+
 class Building(db.Model):
-    __tablename__ = 'buildings'
+    __tablename__ = 'building'
     id = db.Column(db.Integer, primary_key=True)
-    ownerID = db.Column(db.Integer, nullable=False)
+    ownerID = db.Column(db.Integer, nullable=False, default=1234567890)
     name = db.Column(db.String(120), default='No description')
     address = db.Column(db.String(500), default='No description')
     description = db.Column(db.String(500), default='No description')
-    number_of_units = db.Column(db.Integer, nullable=False)
+    number_of_units = db.Column(db.Integer, nullable=False, default=6)
+    building_image = db.Column(db.String(500), default='https://dangerwordfilm.files.wordpress.com/2014/04/coming-soon.png')
+    units = db.relationship('Unit', back_populates='building')
 
-    def __init__(self, ownerID, name, address, description, number_of_units):
+
+    def __init__(self, ownerID, name, address, description, number_of_units, building_image):
         self.ownerID = ownerID
         self.name = name
         self.address = address
         self.description = description
         self.number_of_units = number_of_units
+        self.building_image = building_image
+
     
     def __repr__(self):
         return '<id {}>'.format(self.id)
@@ -39,10 +47,12 @@ class Building(db.Model):
             'address': self.address,
             'description': self.description,
             'number_of_units': self.number_of_units
+            
+            
         }
 
 class Unit(db.Model):
-    __tablename__ = 'units'
+    __tablename__ = 'unit'
     id = id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(500), default='No description')
     number_of_bedroom = db.Column(db.Integer, nullable=False, default=1)
@@ -50,16 +60,21 @@ class Unit(db.Model):
     number_of_bathroom = db.Column(db.Integer, nullable=False, default=1)
     vacancy = db.Column(db.Boolean(), default=True)
     type_of_rental = db.Column(db.String(120), default='Year')
-    rent_price = db.Column(db.Integer, nullable=False)
+    rent_price = db.Column(db.Integer, nullable=False, default=1000)
+    unit_image = db.Column(db.String(500), default='https://dangerwordfilm.files.wordpress.com/2014/04/coming-soon.png')
+    building_id = db.Column(db.Integer, db.ForeignKey('building.id'), nullable=False)
+    building = db.relationship('Building', back_populates='units')
 
-    def __init__(self, description, number_of_bedroom, number_of_living_room, number_of_bathroom, vacancy, type_of_rental, rent_price):
+    def __init__(self, description, number_of_bedroom, number_of_living_room, number_of_bathroom, vacancy, type_of_rental, rent_price, unit_image, building_id):
         self.description = description
         self.number_of_bedroom = number_of_bedroom
         self.number_of_living_room = number_of_living_room
         self.number_of_bathroom = number_of_bathroom
         self.vacancy = vacancy,
-        self.type_of_rental = type_of_rental,
-        self.rent_price = rent_price,
+        self.type_of_rental = type_of_rental
+        self.rent_price = rent_price
+        self.unit_image = unit_image
+        self.building_id = building_id
 
 
     def __repr__(self):
@@ -86,6 +101,8 @@ class Unit(db.Model):
             'vacancy': self.vacancy,
             'type_of_rental': self.type_of_rental,
             'rent_price': self.rent_price,
+            'unit_image': self.unit_image,
+            'building_id': self.building_id,
 
         }
 
